e907d5d09a4ed2408742bbe2b04ae93e5c386d2c
Dhruv Ghinaiya <dhruvghinaiya793@gmail.com>
2025-09-14 12:04:44 +0530

    datatype program

---
+++ Datatype.py
@@ -0,0 +1,11 @@
+name = "dhruv ghinaiya"
+age = 20
+height = 5.7
+is_student = True
+isSalary = None
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(isSalary))
